fafbefa955e5fa2e3f2af702b4b434195878a0bb
amaliekea <amje0001@stud.kea.dk>
2024-09-17 18:13:09 +0200

    initial

--- src/main/java/com/amalie/thymeleaf/touristguide/repository/TouristRepository.java
+++ src/main/java/com/amalie/thymeleaf/touristguide/repository/TouristRepository.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+//feauture branch
 @Repository
 public class TouristRepository {
     private List<TouristAttraction> touristAttractions;
